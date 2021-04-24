8f8a5fc1c3a320fc1facd9315fda53278804b1a0
jagdeepsingh84 <A00222002@mycambrian.ca>
2021-04-23 22:13:54 -0600

    errors solved

---
+++ Jagdeep_Todo_List/Data/Migrations/20210424040757_Migration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Jagdeep_Todo_List.Data.Migrations
+{
+    public partial class Migraion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
